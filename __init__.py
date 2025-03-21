c5679b8bea69592b4336cdffa8aadd80fec4c6a6
kvnl-rgb <80843520+kvnl-rgb@users.noreply.github.com>
2025-03-21 10:45:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template_string, render_template, jsonify, request
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
@@ -9,9 +9,9 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world(): #kaka
-  
     return render_template('hello.html')
 
+# Générer la clé une seule fois et la stocker
 key = Fernet.generate_key()
 f = Fernet(key)
 
@@ -20,6 +20,15 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+    try:
+        token_bytes = token.encode()  # Conversion str -> bytes
+        valeur_decryptee = f.decrypt(token_bytes)  # Decrypt le token
+        return f"Valeur décryptée : {valeur_decryptee.decode()}"
+    except Exception as e:
+        return f"Erreur de décryptage : {str(e)}"
                                                                                                                                                      
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
